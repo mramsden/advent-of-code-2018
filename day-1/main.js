b9f2325412e9caf1c22bc677bcd19a1e4f1fa152
Marcus Ramsden <marcus.ramsden@gmail.com>
2018-12-01 09:26:49 +0000

    [day-1] Frequency calibration

--- day-1/main.js
+++ day-1/main.js
@@ -10,6 +10,67 @@ const calculateFrequency = (currentFrequency, change) => {
     return currentFrequency;
 };
 
-module.exports = {
-    calculateFrequency
+const calculateFrequencyDrift = (readings) =>
+    readings.split('\n').reduce((currentDrift, change) => {
+        return calculateFrequency(currentDrift, change.trim());
+    }, 0);
+
+class ChangeStream {
+
+    constructor(readings) {
+        this.changes = readings.split('\n');
+    }
+
+    next() {
+        const position = this.position || 0;
+        if (position + 1 < this.changes.length) {
+            this.position = position + 1;
+        } else {
+            this.position = 0;
+        }
+        return this.changes[position];
+    }
+
+}
+
+const calibratedFrequency = (readings) => {
+    const observedFrequencies = [0];
+    const stream = new ChangeStream(readings);
+    let change;
+    let currentFrequency;
+    while (change = stream.next()) {
+        currentFrequency = calculateFrequency(currentFrequency || 0, change);
+        if (observedFrequencies.includes(currentFrequency)) {
+            break;
+        }
+        observedFrequencies.push(currentFrequency);
+    }
+
+    return currentFrequency;
 };
+
+module.exports = {
+    calculateFrequency,
+    calculateFrequencyDrift,
+    calibratedFrequency
+};
+
+if (require.main !== module) {
+    return;
+}
+
+const readStdin = () => new Promise(resolve => {
+    let input = '';
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+
+    process.stdin.on('data', chunk => {
+        input += chunk;
+    });
+
+    process.stdin.on('end', () => {
+        resolve(input);
+    });
+});
+
+readStdin().then(input => console.log(calibratedFrequency(input)));
--- day-1/test.js
+++ day-1/test.js
@@ -1,13 +1,37 @@
 const assert = require('assert');
-const { calculateFrequency } = require('./main');
+const { calculateFrequency, calculateFrequencyDrift, calibratedFrequency } = require('./main');
 
 // Current frequency 0, change of +1; resulting frequency 1.
 assert(calculateFrequency(0, '+1') === 1);
+
 // Current frequency 1, change of -2; resulting frequency -1.
 assert(calculateFrequency(1, '-2') === -1);
+
 // Current frequency -1, change of +3; resulting frequency 2.
 assert(calculateFrequency(-1, '+3') === 2);
+
 // Current frequency 2, change of +1; resulting frequency 3.
 assert(calculateFrequency(2, '+1') === 3);
+
+// '+1\n+1\n+1' results in 3
+assert(calculateFrequencyDrift('+1\n+1\n+1') === 3);
+
+// '-1\n-1\n+2' results in 0
+assert(calculateFrequencyDrift('-1\n-1\n+2') === 0);
+
+// '-1\n-2\n-3' results in -6
+assert(calculateFrequencyDrift('-1\n-2\n-3') === -6);
+
+// '+1\n-1' results in 3
+assert(calibratedFrequency('+1\n-1') === 0);
+
+// '+3\n+3\n+4\n-2\-4' results in 10
+assert(calibratedFrequency('+3\n+3\n+4\n-2\n-4') === 10);
+
+// '-6\n+3\n+8\n+5\n-6' results in 5
+assert(calibratedFrequency('-6\n+3\n+8\n+5\n-6') === 5);
+
+// '+7\n+7\n-2\n-7\n-4' results in 14
+assert(calibratedFrequency('+7\n+7\n-2\n-7\n-4') === 14);
 
 console.log('Tests passed');
